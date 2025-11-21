79c38087ae67a18b2fa74fef5c31ca86027bf805
TheNahianLab <thenahianlab@gmail.com>
2025-11-21 23:32:47 +0600

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import "./globals.css";
+import ClientLayout from "@/client-layout";
+import { ViewTransitions } from "next-view-transitions";
+
+export const metadata = {
+  title: "Polite Chaos ",
+  description: "MWT by Polite Chaos",
+  icons: {
+    icon: "/site-logo.png",
+    shortcut: "/site-logo.png",
+    apple: "/site-logo.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ViewTransitions>
+          <ClientLayout>{children}</ClientLayout>
+        </ViewTransitions>
+      </body>
+    </html>
+  );
+}
